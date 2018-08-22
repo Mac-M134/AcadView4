786ca825f6392044bbb6bb026606dbb8a88ead1d
Himanshu <Himanshu.m134@hotmail.com>
2018-08-23 04:00:17 +0530

    Primes Upto N

--- PrimeN.java
+++ PrimeN.java
@@ -10,14 +10,14 @@ class PrimeN{
 			for(int j=2;j<i;j++){
 				if(i%j==0){
 					z=1;
+					break;
 				}
-				if(z==0){
-				System.out.print(i+"\n");
-				break;
 			}
+			if(z==0){
+					System.out.print(i+"\n");
+				}
 		}
 	}
-}
 }
 
 
